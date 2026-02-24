77fb9515e58b689b12a0571fd86df1a92cc1e9ee
Matheus Bertoldo <matheusdeoliveiracjt@gmail.com>
2026-02-24 20:15:19 -0300

    Conectando com mongoDB

--- aula-01-criando-a-api/index.js
+++ aula-01-criando-a-api/index.js
@@ -1,13 +1,33 @@
 import express from "express"
+import mongoose from "mongoose"
+import Game from "./models/Games.js"
+
 const app = express()
 
 // Configurações do app
-app.use(express.json) // Permite uso de json nas operações
+app.use(express.json()) // Permite uso de json nas operações
+
+// Iniciando a conexão com o banco de dados mongodb
+mongoose.connect("mongodb://127.0.0.1:27017/api-the-games")
 
 // Criando rota principal
-app.get("/"), (req, res) => {
-    res.status(200).json({message: "API rodando com sucesso"})
-}
+app.get("/", (req, res) => {
+    const games = [
+        {
+            title: "Game 1",
+            year: "2020",
+            plataform: "PC",
+            price: 20
+        },
+        {
+            title: "Game 2",
+            year: "2022",
+            plataform: "Xbox",
+            price: 30
+        }
+    ]
+    res.status(200).json(games)
+})
 
 // Rodando a API na porta 4000
 const port = 4000
