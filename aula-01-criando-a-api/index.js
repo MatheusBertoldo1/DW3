34ae49381e4de7ee5c5f295253d1b20c043a26c4
Matheus Bertoldo <matheusdeoliveiracjt@gmail.com>
2026-02-24 19:32:38 -0300

    Aula 01 criando API

--- aula-01-criando-a-api/index.js
+++ aula-01-criando-a-api/index.js
@@ -1,6 +1,13 @@
 import express from "express"
-
 const app = express()
+
+// Configurações do app
+app.use(express.json) // Permite uso de json nas operações
+
+// Criando rota principal
+app.get("/"), (req, res) => {
+    res.status(200).json({message: "API rodando com sucesso"})
+}
 
 // Rodando a API na porta 4000
 const port = 4000
